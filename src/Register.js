1ad8245e7e2fc2bc9b58da711055efb2c7fb1e3f
rashmiatulbongirwar <49297175+rashmiatulbongirwar@users.noreply.github.com>
2019-04-09 18:46:53 -0700

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import './App.css';
+import DatePicker from "react-datepicker";
+ 
+import "react-datepicker/dist/react-datepicker.css";
 import { Form, Button} from 'react-bootstrap';
+
+
+
+
+
 class Register extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          startDate: new Date()
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleCh = this.handleCh.bind(this);
+      }
+     
+      handleChange(date) {
+        this.setState({
+          startDate: date
+        });
+      }
+
+      handleCh(event) {
+        this.setState({
+          size: event.target.value
+        });
+      }
+
+
+      
+
 render(){
     return(
-        <div className='container'>
+
+
+<div className='container' align="left">
     <Form>
   <Form.Group controlId="formBasicEmail">
    <Form.Label>User name</Form.Label>
@@ -29,6 +65,69 @@ render(){
   <Form.Group controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicAddress">
+  <Form.Label> Address</Form.Label>
+  <Form.Control type="address" placeholder="Enter your address"/>
+
+
+  <Form.Row>
+  <Form.Group controlId="formBasicCity">
+   <Form.Label>City</Form.Label>
+   <Form.Control type="city" placeholder="Enter city" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicZip">
+   <Form.Label>Zip code</Form.Label>
+   <Form.Control type="zip" placeholder="Enter zipcode" />
+  </Form.Group>
+  <Form.Group controlId="formBasicState">
+   <Form.Label>State</Form.Label>
+   <Form.Control type="State" placeholder="Enter your state" />
+  </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formBasicDob">
+  <Form.Label>Date of birth</Form.Label> 
+  <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+      />
+</Form.Group> 
+
+
+<Form.Group controlId="formBasicChoose">
+<Form.Label> Are you a member of an organization?</Form.Label>
+
+        <p>
+          <label>
+            <input
+              type="radio"
+              value="small"
+              checked={this.state.size === "small"}
+              onChange={this.handleCh}
+            />
+            Yes
+          </label>
+          </p>
+          <p>
+          <label>
+            <input
+              type="radio"
+              value="large"
+              checked={this.state.size === "large"}
+              onChange={this.handleCh}
+            />
+            No
+          </label>
+          </p>
+
+
+</Form.Group>
+
+
+
+  
   </Form.Group>
   <Button variant="primary" type="submit">
     Submit
